7542c65bede1049a2487275436d1edce40d47c3e
HariPrasath1996 <hasathphy96@gmail.com>
2024-05-28 14:51:51 +0530

    media query added

---
+++ script.js
@@ -0,0 +1,10 @@
+// select side nav par element
+let sideNavBar=document.querySelector(".Side-Navbar")
+
+function showSIdeNavBar(){
+    sideNavBar.style.left = "0"
+    
+}
+function closeSIdeNavBar(){
+    sideNavBar.style.left = "-60%"
+}
